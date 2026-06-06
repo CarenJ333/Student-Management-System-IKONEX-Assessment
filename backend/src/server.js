47d808150c2ca17de53f09660221af41e484b7de
Caren <carenjepchumba4@gmail.com>
2026-06-07 01:04:52 +0300

    fixing ommission

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const { testConnection } = require('./config/database');
 const streamsRouter = require('./routes/streams');
